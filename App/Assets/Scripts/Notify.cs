3aadfd73f79491430e60aab5751ae460677ad26b
Johnathon Rexus <22.johnathon.rexus@ksd.org>
2021-09-09 10:12:11 -0700

    Fixing 'Notify' script (still WIP)

--- App/Assets/Scripts/Notify.cs
+++ App/Assets/Scripts/Notify.cs
@@ -12,7 +12,7 @@ public class Notify : MonoBehaviour
         //Remove Notifications that have already been displayed 
         AndroidNotificationCenter.CancelAllDisplayedNotifications();
 
-       //set up Android Notification channel to send messages 
+        //set up Android Notification channel to send messages 
         var c = new AndroidNotificationChannel()
         {
             Id = "channel_id",
@@ -23,23 +23,16 @@ public class Notify : MonoBehaviour
         AndroidNotificationCenter.RegisterNotificationChannel(c);
 
         //Johnathon's Edit: adding public strings for each type of exercise, to be called by notification.Text
-        
-            string exercise1 = "Pushups";
-            string exercise2 = "Squats";
-            string exercise3 = "Lunges";
-            string exercise4 = "Planks";
-            string exercise5 = "Stair Steps";
-            string exercise6 = "Burpees";
-            string exercise7 = "Wall Sits";
-            string exercise8 = "Crunches";
-            string exercise9 = "Bicycle Kicks";
-        
+
+        string[] exercises;
+        string s = "Pushups,Squats,Lunges,Planks,Stair Steps,Burpees,Wall Sits,Crunches,Bicycle Kicks";
+        exercises = s.Split(',');
 
         //Setup The Notification that is going to be sent
         var notification = new AndroidNotification();
         //Johnathon's Edit: Notification Title will say "Here's an exercise for you:"
         notification.Title = "Here's an exercise for you:";
-        notification.Text = "Pushups"; //WIP
+        notification.Text = exercises; //WIP
         notification.FireTime = System.DateTime.Now.AddMinutes(5);
         
         //Send the Notification
