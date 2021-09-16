67e89e43857567ae9b7ee78a25896f3bcfadf068
William Randall <22.william.randall@ksd.org>
2021-09-16 10:08:07 -0700

    worked on adding multiple types of exercises, conditional statement to work with the settings to choose the different types.

--- App/Assets/Scripts/ExerciseArray1.cs
+++ App/Assets/Scripts/ExerciseArray1.cs
@@ -1,18 +1,21 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ExerciseArray1 : MonoBehaviour
 {
-
-    
-
     public Button ExerciseGen;
 
     // Will's Edit: made an array for calisthenic exercises.
     public string[] exercisesCalisthenics = new string[] { "Pushups", "Squats", "Lunges", "Planks", "Stair Steps", "Burpees", "Wall Sits", "Crunches/ Situps", "Bicycle Kicks" };
+    public string[] exercisesDumbells = new string[] { "Bicep Curls", "Hammer Curls", "Shrugs", "Lawnmowers", "Arnold/ Chest Presses", "Lateral Presses", "Shoulder Presses"};
+    string[] exerciseCalisthenicsDumbells;
     public Text ExerciseText;
+    public bool exerciseCalisthenicsSetting; //Wills's Edit: Probably need a Setting script but these bools are here for a placeholder so i can write conditionals.
+    public bool exerciseDumbellsSetting;
+    string Etext;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +27,19 @@ public class ExerciseArray1 : MonoBehaviour
     void OnClick()
     {
         //Will's Edit: generating random exercise
-
-        string Etext = exercisesCalisthenics[Random.Range(0, exercisesCalisthenics.Length)];
+        //Will's Edit: Conditional statement that will work with settings to check which excercise list will be selected.
+        if (exerciseCalisthenicsSetting == true && exerciseDumbellsSetting == false)
+        {
+             Etext = exercisesCalisthenics[Random.Range(0, exercisesCalisthenics.Length)];
+        }
+        else if (exerciseCalisthenicsSetting == false && exerciseDumbellsSetting == true)
+        {
+             Etext = exercisesDumbells[Random.Range(0, exercisesDumbells.Length)];
+        }
+        else if (exerciseCalisthenicsSetting == true && exerciseDumbellsSetting == true)
+        {
+            //exerciseCalisthenicsDumbells = exercisesCalisthenics.Concat<>(exercisesDumbells); // needs more research done to add 2 scripts together
+        }
         int Amount = Random.Range(10, 35);
         //amount = Amount.ToString;
         ExerciseText.text = Etext;
