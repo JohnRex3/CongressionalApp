78faefad3a493a2d1801e6836f4a13258d2797c4
Seth Hadley <22.seth.hadley@ksd.org>
2021-09-20 09:08:59 -0700

    Sound slider code

---
+++ App/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Slider volume;
+    public void Start()
+    {
+        volume.value = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+    }
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+    }
+}
