ce066d705c13c3b65b9ac40d40b3762f0dcffc0c
William Randall <22.william.randall@ksd.org>
2021-10-04 09:30:31 -0700

    added some stuff in scneloaderf and settings 1

--- App/Assets/Scripts/Click.cs
+++ App/Assets/Scripts/Click.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Click : MonoBehaviour
 {
-
+    public GameObject Info;
     public void LoadScene(string sceneName)
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        DontDestroyOnLoad(Info);
         SceneManager.LoadScene(sceneName);
     }
 }
