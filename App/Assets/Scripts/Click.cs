4fd931209290613dd413119972ec238923b18898
William Randall <22.william.randall@ksd.org>
2021-10-06 10:19:40 -0700

    Fixing errors

---
+++ App/Assets/Scripts/Click.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Click : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        //GameObject[] objs = GameObject.FindGameObjectsWithTag("Saved");
+        //DontDestroyOnLoad(this.gameObject);
+    }
+    public void LoadScene(string sceneName)
+    {
+        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneName);
+    }
+}
