a756457c96ccc483dc8a3022e2223643796c3b69
Seth Hadley <22.seth.hadley@ksd.org>
2021-10-06 10:13:06 -0700

    working on fixing errors

--- App/Assets/Scripts/Click.cs
+++ App/Assets/Scripts/Click.cs
@@ -11,16 +11,11 @@ public class Click : MonoBehaviour
         //GameObject[] objs = GameObject.FindGameObjectsWithTag("Saved");
         //DontDestroyOnLoad(this.gameObject);
     }
-<<<<<<< HEAD
     
-    public void LoadScene(string sceneName)
-    {
-=======
 
     public void LoadScene(string sceneName)
     {
 
->>>>>>> 09cae79c4b1eb372a6b0730c53aa5bd46aef1ecf
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(sceneName);
     }
